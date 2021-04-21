52c565b961f48a28300e96d65a75d2369a717237
LaurenceM10 <laurenmontenegro10@gmail.com>
2021-04-20 23:24:18 -0600

    Refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture_tdd/src/app.dart';
  
 void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
-  }
-}
